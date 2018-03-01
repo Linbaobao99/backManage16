c1ec87b812a9dd0c1a555d4f8b3efaed3ecf4f6c
Linbaobao <1327575635@qq.com>
2018-03-01 10:42:00 +0800

    4.element-ui使用

--- src/component/login/Login.vue
+++ src/component/login/Login.vue
@@ -1,13 +1,19 @@
 <template>
     <div>
-        登录
+        <el-switch style="display: block" v-model="value4" active-color="#13ce66" inactive-color="#ff4949" active-text="开" inactive-text="关">
+        </el-switch>
     </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {
+  data() {
+    return {
+      value3: true,
+      value4: true
+    };
+  }
+};
 </script>
 
 <style scoped>
--- src/router/router.js
+++ src/router/router.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 //导入受路由控制的组件
 import Login from '../component/login/Login.vue'
+//导入第三方库
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css'
 
 //使用路由
 Vue.use(VueRouter)
+Vue.use(ElementUI)
 
 //导出路由实例
 export default new VueRouter({
